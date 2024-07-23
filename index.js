683792eb64ea82f07c32101b2e5b3ccee99e74d6
silveiravit <vitorsilveira17@outlook.com>
2024-07-22 22:54:23 -0300

    Atualizado 22/07/2024

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const TaskController = require('./Controller/TaskController')
 const cors = require('cors')
 
 try{
-    mongoose.connect('mongodb+srv://vitorsilveira137:x7G4fR49ySBYR0X2@todolist.c5vwmlv.mongodb.net/?retryWrites=true&w=majority&appName=ToDoList')
+    mongoose.connect('link da conexão mongodb')
 }catch(err){
     console.log(err)
 }
